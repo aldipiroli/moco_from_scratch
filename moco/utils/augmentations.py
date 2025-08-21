cb358dd4b7679ad971b6bfae00c96e614024ec5e
Aldi Piroli <aldipiroli@gmail.com>
2025-08-21 08:02:26 +0200

    updated transforms

--- moco/utils/augmentations.py
+++ moco/utils/augmentations.py
@@ -5,8 +5,16 @@ class MoCoAugmentations:
     def __init__(self, config):
         self.config = config
         self.crop_size = config["DATA"]["augm"]["crop_size"]
-        self.transforms = transforms.Compose([transforms.RandomCrop(size=self.crop_size)])
+
+        self.transforms = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(size=self.crop_size, scale=(0.2, 1.0), antialias=False),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+                transforms.RandomGrayscale(p=0.2),
+                transforms.GaussianBlur(kernel_size=int(0.1 * self.crop_size[0])),
+            ]
+        )
 
     def augment(self, x):
-        x_t = self.transforms(x)
-        return x_t
+        return self.transforms(x)
